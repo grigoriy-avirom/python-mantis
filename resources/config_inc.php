95242092b47b812574ed9da5e9032c5a264083bc
Grigoriy Avirom <88avirom@gmail.com>
2024-04-23 21:33:19 +0300

    Получение почты по протоколу POP3

--- resources/config_inc.php
+++ resources/config_inc.php
@@ -5,6 +5,7 @@ $g_database_name = 'bugtracker';
 $g_db_username = 'root';
 $g_db_password = '';
 $g_use_capcha = off;
+$g_signup_use_capcha = off;
 $g_phpMailer_method = PHPMAILER_METHOD_SMTP;
 $g_smtp_port = '25';
 $g_smtp_host = 'localhost';
