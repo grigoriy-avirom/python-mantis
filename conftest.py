93b93317b104d393caf547fb3b3c51136e6c6972
Grigoriy Avirom <88avirom@gmail.com>
2024-04-23 10:55:59 +0300

    File Transfer Protocol (FTP)2

--- conftest.py
+++ conftest.py
@@ -4,7 +4,6 @@ import os.path
 from fixture.application import Application
 import ftputil
 
-
 fixture = None
 target = None
 
@@ -17,27 +16,30 @@ def load_config(file):
             target = json.load(f)
     return target
 
+
 @pytest.fixture(scope="session")
 def config(request):
     return load_config(request.config.getoption("--target"))
+
 
 @pytest.fixture
 def app(request, config):
     global fixture
     browser = request.config.getoption("--browser")
-    web_config = config['web']
-    web_config2 = config['webadmin']
     if fixture is None or not fixture.is_valid():
-        fixture = Application(browser=browser, base_url=web_config['baseUrl'])
+        fixture = Application(browser=browser, base_url=config['web']['baseUrl'])
     return fixture
 
 
 @pytest.fixture(scope="session", autouse=True)
 def configure_server(request, config):
     install_server_configuration(config['ftp']['host'], config['ftp']['username'], config['ftp']['password'])
+
     def fin():
         restore_server_configuration(config['ftp']['host'], config['ftp']['username'], config['ftp']['password'])
+
     request.addfinalizer(fin)
+
 
 def install_server_configuration(host, username, password):
     with ftputil.FTPHost(host, username, password) as remote:
@@ -46,6 +48,7 @@ def install_server_configuration(host, username, password):
         if remote.path.isfile("config_inc.php"):
             remote.rename("config_inc.php", "config_inc.php.bak")
         remote.upload(os.path.join(os.path.dirname(__file__), "resources/config_inc.php"), "config_inc.php")
+
 
 def restore_server_configuration(host, username, password):
     with ftputil.FTPHost(host, username, password) as remote:
@@ -60,6 +63,7 @@ def stop(request):
     def fin():
         fixture.session.ensure_logout()
         fixture.destroy()
+
     request.addfinalizer(fin)
     return fixture
 
