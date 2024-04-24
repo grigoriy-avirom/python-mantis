281a3847500130444caddea2da9bf0cfde959669
Grigoriy Avirom <88avirom@gmail.com>
2024-04-24 22:05:34 +0300

    Получение почты по протоколу POP3 (2)

--- fixture/mail.py
+++ fixture/mail.py
@@ -8,7 +8,7 @@ class MailHelper:
     def __init__(self, app):
         self.app = app
 
-    def get_mail(self,username, password, subject):
+    def get_mail(self, username, password, subject):
         for i in range(5):
             pop = poplib.POP3(self.app.config['james']['host'])
             pop.user(username)
@@ -16,11 +16,11 @@ class MailHelper:
             num = pop.stat()[0]
             if num > 0:
                 for n in range(num):
-                    msglines = pop.retr(n+1)[1]
-                    msgtext = "\n".join(map(lambda x: x.decode("utf-8"), msglines))
+                    msglines = pop.retr(n + 1)[1]
+                    msgtext = "\n".join(map(lambda x: x.decode('utf-8'), msglines))
                     msg = email.message_from_string(msgtext)
-                    if msg.get('Subject') == subject:
-                        pop.dele(n+1)
+                    if msg.get("Subject") == subject:
+                        pop.dele(n + 1)
                         pop.quit()
                         return msg.get_payload()
             pop.close()
---
+++ fixture/soap.py
@@ -0,0 +1,10 @@
+# from suds.client import Client
+# from suds import WebFault
+#
+# class SoapHelper:
+#
+#     def __init__(self, app):
+#         self.app = app
+#
+#     def can_login(self, username, password):
+#         client = Client("")
